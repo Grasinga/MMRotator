37175083acdba3894c7499f05b338668a716cc56
Grasinga <Grasinga@gmail.com>
2014-07-30 03:57:49 -0600

    Wooden Stairs Rotator
    The stairs rotate like they should but my commands aren't working.

--- src/main/java/com/minemee/MMRotator/MMRotatorBlockListener.java
+++ src/main/java/com/minemee/MMRotator/MMRotatorBlockListener.java
@@ -1,8 +1,7 @@
 package com.minemee.MMRotator;
 
-import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
-import org.bukkit.World;
 import org.bukkit.block.Block;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
@@ -14,47 +13,54 @@ import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 
 public class MMRotatorBlockListener implements Listener {
-	private boolean active = false;
-	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args){
+	private boolean active = true;
+	private int count = 0;
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
 		Player player = (Player) sender; 	
-		if(commandLabel.equalsIgnoreCase("mmrotateon")){
-			player.sendMessage("Rotator Tool Activated");
+		if(label.equalsIgnoreCase("rotateon")){
+			player.sendMessage(ChatColor.GREEN + "Rotator Tool Activated");
 			active = true;
 			return true;
 		}
-		else if(commandLabel.equalsIgnoreCase("mmrotateoff"))
+		else if(label.equalsIgnoreCase("rotateoff"))
 		{
-			player.sendMessage("Rotator Tool De-activated");
+			player.sendMessage(ChatColor.RED + "Rotator Tool De-activated");
 			active = false;
+			return true;
+		}
+		else if(label.equalsIgnoreCase("rotateon") || label.equalsIgnoreCase("rotateoff") && (!(sender instanceof Player)))
+		{
+			sender.sendMessage("This command can only be used by a player.");
 			return true;
 		}
 		return false;
 	}
+	@SuppressWarnings("deprecation")
 	@EventHandler(priority = EventPriority.NORMAL)
 	public void stickChange(PlayerInteractEvent event){	
 		final Player player = event.getPlayer();
-		if(active)
-		{
-			int count = 0;
 			if(event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK)
 			{
-				if(player.getItemInHand().getType().equals(Material.STICK))
+				if(active)
 				{
-					Block block = event.getClickedBlock();
-					if(count == 0)
-						block.setTypeIdAndData(53, (byte) 0x0, false);
-					if(count == 1)
-						block.setTypeIdAndData(53, (byte) 0x1, false);
-					if(count == 2)
-						block.setTypeIdAndData(53, (byte) 0x2, false);
-					if(count == 3)
+					if(player.getItemInHand().getType().equals(Material.STICK))
 					{
-						block.setTypeIdAndData(53, (byte) 0x3, false);
-						count = -1;
+						Block block = event.getClickedBlock();
+						if(count == 0)
+							block.setTypeIdAndData(53, (byte) 0x0, false);
+						if(count == 1)
+							block.setTypeIdAndData(53, (byte) 0x1, false);
+						if(count == 2)
+							block.setTypeIdAndData(53, (byte) 0x2, false);
+						if(count == 3)
+						{
+							block.setTypeIdAndData(53, (byte) 0x3, false);
+							count = -1;
+						}
+						count++;
 					}
-					count++;
 				}
 			}
-		}
+		
 	}
 }
